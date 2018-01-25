4ead93efc04027c317619b34de24fba1c228af48
Andrey Golubev <AndreyGolubev85@gmail.com>
2018-01-26 00:09:11 +1100

    2

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,20 @@
+package controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+//@RequestMapping("/hello")
+@RequestMapping("/")
+public class IndexController {
+@RequestMapping(method = RequestMethod.GET)
+    //@GetMapping("/")
+    public String index(Model m) {
+        m.addAttribute("someAttribute", "someValue");
+        return "index";
+    }
+}
